abaff47e919992bdee034e8ebd3e714cefdff331
Mike Shearer <shearerbeard@gmail.com>
2023-08-31 14:30:28 -0600

    first GRPC test run to service node for adder

--- orchestrator/src/client.rs
+++ orchestrator/src/client.rs
@@ -7,26 +7,34 @@ use equation::{
 };
 use tonic::transport::Channel;
 
+const SCHEME: &str = "http://";
+
 pub(crate) async fn build_adder_client(
     config: &Config,
 ) -> Result<AdderClient<Channel>, tonic::transport::Error> {
-    AdderClient::connect(config.adder_addr.clone()).await
+    AdderClient::connect(build_url(&config.adder_addr)).await
 }
 
 pub(crate) async fn build_subtractor_client(
     config: &Config,
 ) -> Result<SubtractorClient<Channel>, tonic::transport::Error> {
-    SubtractorClient::connect(config.subtractor_addr.clone()).await
+    SubtractorClient::connect(build_url(&config.subtractor_addr)).await
 }
 
 pub(crate) async fn build_multiplier_client(
     config: &Config,
 ) -> Result<MultiplierClient<Channel>, tonic::transport::Error> {
-    MultiplierClient::connect(config.multiplier_addr.clone()).await
+    MultiplierClient::connect(build_url(&config.multiplier_addr)).await
 }
 
 pub(crate) async fn build_divider_client(
     config: &Config,
 ) -> Result<DividerClient<Channel>, tonic::transport::Error> {
-    DividerClient::connect(config.divider_addr.clone()).await
+    DividerClient::connect(build_url(&config.divider_addr)).await
+}
+
+fn build_url(conn_str: &str) -> String {
+    let mut url = SCHEME.to_string();
+    url.push_str(conn_str);
+    url
 }
--- orchestrator/src/main.rs
+++ orchestrator/src/main.rs
@@ -9,10 +9,36 @@ mod client;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = Config::new();
 
-    let mut adder_client = build_adder_client(&config);
-    let mut subtractor_client = build_subtractor_client(&config);
-    let mut multiplier_client = build_multiplier_client(&config);
-    let mut divider_client = build_divider_client(&config);
+    let mut adder_client = build_adder_client(&config).await?;
+    let mut subtractor_client = build_subtractor_client(&config).await?;
+    let mut multiplier_client = build_multiplier_client(&config).await?;
+    let mut divider_client = build_divider_client(&config).await?;
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use equation::proto::equation::{
+        CalculationRequest, CalculationResponse, Uuid
+    };
+
+    #[actix_rt::test] 
+    async fn test_adder() {
+        let config = Config::new();
+        let mut client = build_adder_client(&config).await.unwrap();
+
+        let id = "XXXXXXXXXXXXXXXX";
+
+        let request = tonic::Request::new(CalculationRequest {
+            id: Some(Uuid{ value: id.to_string() }),
+            first_arg: 1,
+            second_arg: 2,
+        });
+
+        let res = client.add(request).await.unwrap();
+
+        println!("RES: {:#?}", res);
+    }
 }
